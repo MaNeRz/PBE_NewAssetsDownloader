74de7c98f0c347e8ba24e8285df8374b6255feab
NetuWasTaken <mariorazvan02@gmail.com>
2021-11-30 23:52:33 +0100

    Moved all the process to the new class Process.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
-﻿using System.Drawing;
-using System.Threading.Tasks;
-using Colorful;
+﻿using System.Threading.Tasks;
 
 namespace PBE_NewFileExtractor
 {
-    internal class Program
+    internal static class Program
     {
         private static async Task Main(string[] args)
         {
-            var directoriesCreator = new DirectoriesCreator();
-            var filesCreator = new FilesCreator();
-            var filesComparator = new FilesComparator();
-            var filesDownloader = new FilesDownloader();
-
-            ApplicationInfos.Infos();
-            await directoriesCreator.CreateDirResourcesAsync();
-            await directoriesCreator.CreateDirAssetsDownloadedAsync();
-            await filesCreator.CreateFilesExtractedAsync();
-            await filesCreator.CreateNewFilesExtractedAsync();
-            await filesComparator.CheckFilesDiffAsync();
-            await filesDownloader.DownloadAssetsAsync();
-            filesComparator.ReplaceAssetsFile();
-            Console.WriteLine("\n\nPress any key to close the program...", Color.Aqua);
-
-            Console.ReadKey();
+            await new Process().MainProcess();
         }
     }
 }
