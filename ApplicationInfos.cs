8754c076e14e2bcfe0ab72e787382a8b67298983
NetuWasTaken <mariorazvan02@gmail.com>
2021-12-01 23:58:59 +0100

    Made the class static and changed the version to Beta 1.2

--- ApplicationInfos.cs
+++ ApplicationInfos.cs
@@ -2,11 +2,11 @@
 
 namespace PBE_NewFileExtractor
 {
-    public class ApplicationInfos
+    public static class ApplicationInfos
     {
         public static void Infos()
         {
-            const string version = "Beta 1.1";
+            const string version = "Beta 1.2";
             Console.Title = $"League Of Legends - PBE New Assets Extractor | v.{version}";
         }
     }
